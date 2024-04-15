b14a92f9ebd9ed0b20df9598abe736c9a560ab7c
Saksham Gulati <sakshamgulati@geotab.com>
2024-04-15 18:25:59 -0400

    added inference pipeline

--- src/ModelOps/__init__.py
+++ src/ModelOps/__init__.py
@@ -1,2 +1,3 @@
 #import all modules in the ModelOps package
 from .model_fit import ModelFit
+from .model_inference import ModelInference
--- src/ModelOps/model_inference.py
+++ src/ModelOps/model_inference.py
@@ -6,7 +6,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 import pickle
 import os
 
-class ModelFit:
+class ModelInference:
     """
     #docstring for ModelFit class
     #This class is used to train the model and log the metrics to weights and biases
@@ -16,13 +16,13 @@ class ModelFit:
     def __init__(self):
         os.environ["WANDB_API_KEY"] = "22787bdec6329d031c43de72471e610b908a8815"
         # os.environ["WANDB_API_KEY"]  = os.getenv('WANDB_API_KEY')
-        self.run = wandb.init(project="ml-ops-template")
+        self.run = wandb.init(project="ml-ops-template",job_type="inference")
         logging.info(f"Weights and Biases initiated with Run ID: {self.run.id}")
         logging.info(
             f"For more information on the experiments visit: https://wandb.ai/sakshamgulati123"
         )
 
-    def model(self, X_train, X_test, y_train, y_test):
+    def inference(self, y_test):
         """
         #docstring for model
         #This function is used to train the model and log the metrics to weights and biases
@@ -30,14 +30,13 @@ class ModelFit:
         #Output: Model object
 
         """
-        reg = RandomForestRegressor().fit(X_train, y_train)
-        logging.info("Model Trained")
-
-        wandb.log({"Train R-squared": reg.score(X_train, y_train)})
-        logging.info(f"R squared logged:{reg.score(X_train, y_train)}")
-        y_pred = reg.predict(X_test)
-        assert len(y_pred) == len(y_test), "Length mismatch"
-
+        artifact = self.run.use_artifact('sakshamgulati123/ml-ops-template/model_atzgbhx7:v0', type='model')
+        artifact_dir = artifact.download()
+        #load  pickle file
+        with open(artifact_dir, 'rb') as f:
+            model = pickle.load(f)
+        logging.info("Model loaded from the registry")
+        y_pred = model.predict(y_test)
         mse = mean_squared_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
         # logging MSE and R2 score
@@ -45,27 +44,4 @@ class ModelFit:
         logging.info(f"R2:{r2}")
         wandb.log({"Mean squared error": mse, "Test R-squared": r2})
         
-        return reg
-
-    def save_model_to_registry(self,model,wandb=True):
-        """
-        #docstring for save_model_to_registry
-        #This function is used to save the model to the registry
-        #Input: None
-        #Output: None
-
-        """
-        INIT_MODEL_DIR='init_model'
-        
-        if wandb:
-            
-            artifact = wandb.Artifact(name="my_models", type="model")
-            model.save(INIT_MODEL_DIR)
-            artifact.add_dir(INIT_MODEL_DIR)
-            self.run.log_artifact(artifact)
-            logging.info("Model saved to weights and biases registry")
-        else:
-            logging.info("Model not saved to weights and biases registry")
-        self.run.finish()
-        return None
-
+        return y_pred
