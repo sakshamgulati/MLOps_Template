5ab55f3725f814c010366a871a59b4bd30521b47
Saksham Gulati <sakshamgulati@geotab.com>
2024-06-15 16:31:00 -0400

    added model monitoring for inference

--- src/ModelOps/model_inference.py
+++ src/ModelOps/model_inference.py
@@ -48,5 +48,40 @@ class ModelInference:
             model = pickle.load(f)
         logging.info("Model loaded from the registry")
         y_pred = model.predict(X_test)
-        self.run.finish()
         return y_pred
+    
+    def monitoring(self):
+        """
+        #docstring for model
+        #This function is used to train the model and log the metrics to weights and biases
+        #Input: X_train, X_test, y_train, y_test
+        #Output: Model object
+        """
+        from evidently.metrics import RegressionQualityMetric, RegressionErrorPlot, RegressionErrorDistribution
+        from evidently.metric_preset import DataDriftPreset, RegressionPreset
+        from evidently.pipeline.column_mapping import ColumnMapping
+        from evidently.report import Report
+        target = 'y'
+        prediction = 'prediction'
+        # numerical_features = ['open', 'high', 'low', 'adjusted close', 'volume', 'dividend amount']
+        # categorical_features = ['season', 'holiday', 'workingday', ]#'weathersit']
+
+        column_mapping = ColumnMapping()
+
+        column_mapping.target = target
+        column_mapping.prediction = prediction
+        # column_mapping.datetime_features = 'ds'
+        column_mapping.datetime='ds'
+        column_mapping.id = None
+                          
+        regression_performance_report = Report(metrics=[
+            RegressionPreset(),
+        ])
+        # get the reference dataset
+        regression_performance_report.run(reference_data=None, current_data=reference,
+                                        column_mapping=column_mapping)
+        regression_performance_report
+        logging.info("Model performance monitored")
+        self.run.finish()
+        
+        return None
